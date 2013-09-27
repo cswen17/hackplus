125ef4ab5b1e691f02f77002dee4f2f4a52e895a
cswen17 <cswen@andrew.cmu.edu>
2013-09-27 19:55:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackplus::Application.config.secret_token = '00f88681c43bd73ccae4c30c854d1a09024a7eaff327ef622dba43d0a48a3144970c0ee02e14a71917a4eda63999e6f72ae1b7126d9d13cab1d24294554d8a35'
