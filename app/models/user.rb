ece094f2e829384ab301919f383a86b7fdfd98e9
James Wu <jameswu@cmu.edu>
2013-10-01 00:56:40 -0400

    Spiced up registration page with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #found it online, basic checking for format
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 
   has_secure_password
 end
