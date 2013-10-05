8859d277519eb58fdd2a17f551247c6e792ec0ef
James Wu <jameswu@cmu.edu>
2013-10-05 14:14:10 -0400

    Fixed bug with tasks where time was not showing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,12 @@ class TasksController < ApplicationController
 	def create
 		@task = Task.new(params[:task])
 		if @task.save
+			#make time attribute follow this pattern instead
+			@task.update_attributes(:duetime => DateTime.strptime(params[:task][:duetime], "%m/%d/%Y %H:%M:%S %p"))
 			@user = User.find(params[:task][:id])
 
 			@user.tasks.create(params[:task])
+
 			flash[:success] = "Task Saved."
 			redirect_to root_url
 		else
