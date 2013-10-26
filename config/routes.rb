3f728ee6e1397377a60745d1ba3a659a58ec7a5f
Jacob <jacob@jacob-GT60.(none)>
2013-10-25 20:18:40 -0400

    fixed routing bugs with completing tasks

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Hackplus::Application.routes.draw do
   resources :users do
     resources :tasks
   end
-  resources :tasks
+  resources :tasks do
+    put 'complete', on: :member
+  end
   
   resources :sessions, only: [:new, :create, :destroy]
   
@@ -21,7 +23,7 @@ Hackplus::Application.routes.draw do
   match '/signin' => 'sessions#new'
   match 'users/:id/newtask' => 'tasks#new'
   match 'newtask' => 'tasks#new'
-  match '/complete.:id' => 'tasks#complete'
+  match 'tasks/:id/complete' => 'tasks#complete'
 
   match '/tasks/:id/' => 'tasks#index'
 
